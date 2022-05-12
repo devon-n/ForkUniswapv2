25a0701d5b28bd0122731807c9625410fc1bc28e
Devon Nathan <devnathan94@gmail.com>
2022-05-12 16:32:39 +1000

    init

---
+++ migrator/contracts/LiquidityMigrator.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.6.6;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
+import './IUniswapV2Pair.sol';
+import './BonusToken.sol';
+
+contract LiquidityMigrator {
+    IUniswapV2Router02 public router;
+    YOU ARE HERE 36:46
+}
